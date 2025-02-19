8ff7cf9281010b75c45fc90a7a3b28cfac549e99
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-19 21:15:14 +0530

    adding packages to install

--- frontend.sh
+++ frontend.sh
@@ -51,7 +51,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip 
 validate $? "unzip the frontend code"
 
-cp home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 validate $? "copying expense conf"
 
 systemctl restart nginx 
